29899cf30683c6acbfe7debed8821035a771e1be
Nereiis <nerearegsan.pro@gmail.com>
2023-07-11 14:42:29 +0200

    chekSession

--- StreamHub/src/app/pages/check-session/check-session.component.ts
+++ StreamHub/src/app/pages/check-session/check-session.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { LibrosI, PeliculasI, SeriesI, UsersI } from 'src/app/models/interfaces';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { LibrosService } from 'src/app/services/libros/libros.service';
+import { PeliculasService } from 'src/app/services/peliculas/pelicula.service';
+import { SeriesService } from 'src/app/services/series/series.service';
 
 @Component({
   selector: 'app-check-session',
@@ -6,5 +11,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./check-session.component.scss']
 })
 export class CheckSessionComponent {
+  
+  user!: UsersI;
+  seriesFav: string[] = []; 
+  pelisFav: string[] = [];
+  librosFav: string[] = [];
+  
+
+  constructor( public authService: AuthService ) {}
+
+  ngOnInit(): void{
+    this.user = JSON.parse(String(this.authService.getCurrentUser()));
+    this.authService.getUserById(this.user._id).subscribe((data:any) => {
+
+      this.seriesFav = data.SeriesFavoritas;
+      this.pelisFav = data.PeliculasFavoritas;
+      this.librosFav = data.LibrosFavoritos;
+      console.log(this.seriesFav);
+      console.log(this.pelisFav);
+      console.log(this.librosFav);    
+      
+      
+    })
+  }
+
+
+
 
 }
