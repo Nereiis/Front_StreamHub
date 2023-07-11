5faccfed5b9e626644f8cd702ef0757cfa5c370f
XavierMacias <xmaciasxicola@gmail.com>
2023-07-11 14:50:08 +0200

    favoritos en check session

--- StreamHub/src/app/pages/check-session/check-session.component.ts
+++ StreamHub/src/app/pages/check-session/check-session.component.ts
@@ -16,9 +16,12 @@ export class CheckSessionComponent {
   seriesFav: string[] = []; 
   pelisFav: string[] = [];
   librosFav: string[] = [];
-  
 
-  constructor( public authService: AuthService ) {}
+  libros: LibrosI[] = [];
+  series: SeriesI[] = [];
+  pelis: PeliculasI[] = [];
+
+  constructor( public authService: AuthService, public libroService: LibrosService, public serieService: SeriesService, public pelisService: PeliculasService ) {}
 
   ngOnInit(): void{
     this.user = JSON.parse(String(this.authService.getCurrentUser()));
@@ -31,7 +34,23 @@ export class CheckSessionComponent {
       console.log(this.pelisFav);
       console.log(this.librosFav);    
       
-      
+      for(let libroId of this.librosFav) {
+        this.libroService.getLibroById(libroId).subscribe((data:any) => {
+          this.libros.push(data);
+        })
+      }
+
+      for(let serieId of this.seriesFav) {
+        this.serieService.getSerieById(serieId).subscribe((data:any) => {
+          this.series.push(data);
+        })
+      }
+
+      for(let peliId of this.pelisFav) {
+        this.pelisService.getPeliculaById(peliId).subscribe((data:any) => {
+          this.pelis.push(data);
+        })
+      }
     })
   }
 
